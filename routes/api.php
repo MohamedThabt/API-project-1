f2cc75be0c7c20c7d186aacaed5118b0504b56cb
MOhamed Thabet <mohamedthabetabdelhalem_sd23@fcis.bsu.edu.eg>
2024-12-09 13:01:27 +0200

    Refactor API routes to improve organization and RESTful compliance

--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,42 @@ use App\Http\Controllers\LikeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public Routes
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+});
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-//no need to be authenticated to access this route
+// No need to be authenticated to access this route
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('/post/{id}', [PostController::class, 'getPost']);
+Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
 
-
-
-//this route group will be protected by sanctum(need token to access)
+// Protected Routes (requires sanctum authentication)
 Route::middleware('auth:sanctum')->group(function () {
-    //Blog api routes
-    Route::post('/add/post', [PostController::class, 'addPost']);
-    Route::post('/edit/post', [PostController::class, 'editPost']);
-    Route::delete('/post/{id}', [PostController::class, 'deletePost']);
-    //comment api routes
-    Route::post('/comment', [CommentController::class, 'postComment']);
-    //Like aoi routes 
-    Route::post('/like', [LikeController::class, 'addLike']);
-    Route::delete('/like/{id}', [LikeController::class, 'deleteLike']);
+    // User Information
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->name('auth.user');
 
-    // Logout route
-    Route::post('/logout', [AuthController::class, 'logout']);
+    // Blog Routes
+    Route::prefix('posts')->group(function () {
+        Route::post('/', [PostController::class, 'store'])->name('posts.store');
+        Route::put('/{id}', [PostController::class, 'update'])->name('posts.update'); // Changed to PUT for RESTful compliance
+        Route::delete('/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    });
+
+    // Comment Routes
+    Route::prefix('comments')->group(function () {
+        Route::post('/', [CommentController::class, 'store'])->name('comments.store');
+        Route::post('/', [CommentController::class, 'destroy'])->name('comments.destroy');
+    });
+
+    // Like Routes
+    Route::prefix('likes')->group(function () {
+        Route::post('/', [LikeController::class, 'store'])->name('likes.store');
+        Route::delete('/{id}', [LikeController::class, 'destroy'])->name('likes.destroy');
+    });
+
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
